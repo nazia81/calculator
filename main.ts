836e8ca406204b87ec8c535de850ab342a6c7bab
nazia81 <160647694+nazia81@users.noreply.github.com>
2024-04-22 01:13:08 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "enter your first number",type:"number" ,name:"firstnumber"},
+  {message:"enter your second number",type:"number",name:"secondnumber"},
+  {message:"select one of the following operator to perform action",type:"list",name: "operator",choices:["+","-","*","/"]},
+]);
+//conditional statements,
+if (answer.operator ==="+")
+console.log(answer.firstnumber + answer.secondnumber);
+else if(answer.operator ==="-")
+  console.log(answer.firstnumber - answer.secondnumber);
+else if(answer.operator ==="*")
+  console.log(answer.firstnumber *answer.secondnumber)
+else if(answer.operator ==="/")
+  console.log(answer.firstnumber / answer.secondnumber)
+else{console.log("please select a valid operator")
+  
+}
